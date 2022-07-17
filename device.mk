be5d948a68c4e99821460a99977929206d218bff
Nischay Sanjay <nischay.sanjay23@gmail.com>
2022-07-17 19:52:30 +0530

    tulip: Build missing vendor.qti.hardware.btconfigstore@2.0
    Could not find 'vendor.qti.hardware.btconfigstore@2.0::IBTConfigStore/default' for ctl.interface_start from pid: 562 (/system/bin/hwservicemanager)

--- device.mk
+++ device.mk
@@ -137,6 +137,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
+    vendor.qti.hardware.btconfigstore@2.0 \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
 # QTI Bluetooth
